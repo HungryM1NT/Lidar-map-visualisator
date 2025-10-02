0b387b9f1a8546efd4aa4c218c54700803b1b2f8
M1NT <ilya.ablyazov.2004@gmail.com>
2025-10-02 20:27:53 +0300

    readme

--- python_pcd/main.py
+++ python_pcd/main.py
@@ -2,8 +2,9 @@ import open3d as o3d
 import numpy as np
 
 def main():
+    file_name = "../assets/office1.pcd"
     pcd = o3d.io.read_point_cloud("../assets/office1.pcd")
-    o3d.io.write_point_cloud("hak_binary2.pcd", pcd, compressed=False, write_ascii=False)
+    o3d.io.write_point_cloud("../assets/new_binary.pcd", pcd, compressed=False, write_ascii=False)
 
 if __name__ == "__main__":
     main()
