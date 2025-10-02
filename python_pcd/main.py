f30fc45afaef0d42290ccc00a530dff62903a5eb
HungryM1NT <ilya.ablyazov.2004@gmail.com>
2025-10-02 16:07:12 +0300

    code clearing

--- python_pcd/main.py
+++ python_pcd/main.py
@@ -2,25 +2,8 @@ import open3d as o3d
 import numpy as np
 
 def main():
-    pcd = o3d.t.io.read_point_cloud("../assets/hak_big/hak_ascii.pcd")
-    # pcd = o3d.io.read_point_cloud("../assets/office1.pcd")
-    # print(pcd)
-    o3d.visualization.draw([pcd])
-    
-    # mpoints = np.array([
-
-    # ])
-    # mpoints = []
-    # for point in pcd.points:
-    #     # print([float(point[0]), float(point[1]), float(point[2]) - 220])
-    #     mpoints.append([point[0], point[1], point[2] - 220])
-    #     # point -= 220
-    #     # print(point)
-    #     # print(mpoints)
-    # # print("Hello from python-pcd!")
-    # pcd = o3d.geometry.PointCloud()
-    # pcd.points = o3d.utility.Vector3dVector(mpoints)
-    # o3d.io.write_point_cloud("hak_binary2.pcd", pcd, compressed=False, write_ascii=True)
+    pcd = o3d.io.read_point_cloud("../assets/office1.pcd")
+    o3d.io.write_point_cloud("hak_binary2.pcd", pcd, compressed=False, write_ascii=False)
 
 if __name__ == "__main__":
     main()
